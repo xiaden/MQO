19e9d281a509bf038f7bb5b4f39eec04bc942087
xiaden <xiaden@users.noreply.github.com>
2017-04-23 16:15:15 -1000

    Update and rename TooltipFix.js to TooltipFix.user.js

--- TooltipFix.user.js
+++ TooltipFix.user.js
@@ -4,8 +4,8 @@
 // @description Fixes tooltips so they overlap correctly.
 // @homepage https://github.com/xiaden/MQO
 // @version 1
-// @downloadURL  https://xiaden.github.io/MQO/TooltipFix.js
-// @updateURL    https://xiaden.github.io/MQO/TooltipFix.js
+// @downloadURL  https://xiaden.github.io/MQO/TooltipFix.user.js
+// @updateURL    https://xiaden.github.io/MQO/TooltipFix.user.js
 // @include http://midenquest.com/Game.aspx
 // @include http://www.midenquest.com/Game.aspx
 // @include https://www.midenquest.com/Game.aspx
